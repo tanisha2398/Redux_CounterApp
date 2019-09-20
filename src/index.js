c0ce45c0d4dc30145e47321b95c18907f07fc883
tanisha2398 <tanishanegipro@gmail.com>
2019-09-21 00:52:50 +0530

    add store to browser through composeEnhancer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
@@ -23,7 +23,11 @@ const logger = store => {
     };
   };
 };
-const store = createStore(rootReducer, applyMiddleware(logger));
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancer(applyMiddleware(logger))
+);
 ReactDOM.render(
   <Provider store={store}>
     <App />
