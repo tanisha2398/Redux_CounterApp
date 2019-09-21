71b108c9084607073a76b60198ddb4d02a782cdc
tanisha2398 <tanishanegipro@gmail.com>
2019-09-21 11:52:59 +0530

    utility fn to make reducer clean

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "../actions/actions";
+import { updateObject } from "../utility";
 const initialState = {
   counter: 0
 };
@@ -15,10 +16,11 @@ const reducer = (state = initialState, action) => {
         counter: state.counter - 1
       };
     case actionTypes.ADD:
-      return {
-        ...state,
-        counter: state.counter + action.val
-      };
+      return updateObject(state, { counter: state.counter + action.val });
+    // return {
+    //   ...state,
+    //   counter: state.counter + action.val
+    // };
     case actionTypes.SUBTRACT:
       return {
         ...state,
